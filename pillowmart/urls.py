5a605a8562e50a056b6dd18617e4c46b7e124703
Mikhail Prorekhin <mikhail.prorekhin@gmail.com>
2025-07-09 15:09:43 +1000

    added fixures + debug-tool

--- pillowmart/urls.py
+++ pillowmart/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
+from pillowmart import settings
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main')),
-    path('catalog/', include('products.urls', namespace='catalog')),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls", namespace="main")),
+    path("catalog/", include("products.urls", namespace="catalog")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
